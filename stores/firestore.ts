8092ba0435e6ca0d095c2b2f7f5124bd1c242d4b
kristoffer <kunkristoffer@gmail.com>
2024-10-11 13:49:27 +0200

    Removed comments

--- stores/firestore.ts
+++ stores/firestore.ts
@@ -6,28 +6,18 @@ export const useItemStore = defineStore('itemStore', () => {
 
   const updateData = (config: dndRarity) => {
     const { mundane = 0, common = 0, uncommon = 0, rare = 0, veryrare = 0, legendary = 0, artifact = 0 } = config
-
-    // Copy data to avoid mutation
     const oldData: dndItem[] = JSON.parse(JSON.stringify(testData))
-
-    // shuffle position of items to simulate random draw later
     const randomized: dndItem[] = oldData.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value)
-
-    // Convert array into object with rarity as keys
     const reduced = randomized.reduce<{[key: string]: dndItem[]}>((acc, cur) => {
       const key = cur.rarity.replace(/\s/g,'').toLocaleLowerCase()
       acc[key] = acc[key] || []
       acc[key].push(cur)
       return acc
     }, {})
-
-    // trim array length to limit items for each rarity
     for (const key in reduced) {
       // fix type error
       reduced[key].length = config[key]
     }
-
-    // flatten object values into single dimention array
     const flatten = Object.values(reduced).flat()
     data.value = flatten
   }
