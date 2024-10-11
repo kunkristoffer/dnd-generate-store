ec37cc1a2ff7c377ececde8d92ad78cc2572c089
kristoffer <kunkristoffer@gmail.com>
2024-10-11 12:03:54 +0200

    Added login code

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+  import { GoogleAuthProvider } from 'firebase/auth'
+  import { signInWithPopup } from 'firebase/auth'
+  import { useFirebaseAuth } from 'vuefire'
+
+  const googleAuthProvider = new GoogleAuthProvider()
+  const auth = useFirebaseAuth()!
+
+  function signinPopup() {
+    signInWithPopup(auth, googleAuthProvider).catch((reason) => {
+      console.error('Failed sign', reason)
+    }).then(async() => await navigateTo('/'))
+  }
+</script>
+
 <template>
-  <h1>login</h1>
+  <main class="relative grid place-content-center flex-1 p-4 bg-slate-700 text-white">
+    <button class="px-8 py-2 rounded-lg bg-slate-800 hover:bg-slate-900 active:bg-slate-500" @click="signinPopup()">Sign-in with Google</button>
+  </main>
 </template>
