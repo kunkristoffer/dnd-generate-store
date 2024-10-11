7df84c396bd57ed54dbee0ba0d2ac473832b98cb
kristoffer <kunkristoffer@gmail.com>
2024-10-11 14:53:46 +0200

    Firestore connected, swaping datasets

--- stores/itemStore.ts
+++ stores/itemStore.ts
@@ -1,16 +1,21 @@
-import testData from '~/assets/dnditems.json'
+// import testData from '~/assets/dnditems.json'
 import type { dndItem, dndItemBase, dndRarity } from '~/types/dnditem';
-import { doc, getDoc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore'
+import { doc, collection, getDoc, serverTimestamp, setDoc, updateDoc, CollectionReference } from 'firebase/firestore'
 
 export const useItemStore = defineStore('itemStore', () => {
   const db = useFirestore()
   const user = useCurrentUser()
 
-  const data = ref<dndItem[]>(JSON.parse(JSON.stringify(testData)))
+  const itemData = useCollection<CollectionReference<dndItem[]>>(collection(db, 'items'))
+  const baseData = useCollection<CollectionReference<dndItem[]>>(collection(db, 'base'))
 
+  // temp dataset due to mutation
+  const data: Ref<dndItem[]> = ref([])
+
+  // Update data with user input
   const updateData = (config: dndRarity) => {
     const { mundane = 0, common = 0, uncommon = 0, rare = 0, veryrare = 0, legendary = 0, artifact = 0 } = config
-    const oldData: dndItem[] = JSON.parse(JSON.stringify(testData))
+    const oldData: dndItem[] = JSON.parse(JSON.stringify(itemData.value))
     const randomized: dndItem[] = oldData.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value)
     const reduced = randomized.reduce<{[key: string]: dndItem[]}>((acc, cur) => {
       const key = cur.rarity.replace(/\s/g,'').toLocaleLowerCase()
@@ -26,10 +31,8 @@ export const useItemStore = defineStore('itemStore', () => {
     data.value = flatten
   }
 
-  const pickRandom = () => {
-    const index = randomBetween(0, testData.length)
-    data.value = [JSON.parse(JSON.stringify(testData[index]))]
-  }
+  // Pick a random item
+  const pickRandom = () => data.value = [JSON.parse(JSON.stringify(itemData.value[randomBetween(0, itemData.value.length)]))]
 
   return { data, updateData, pickRandom}
 })
