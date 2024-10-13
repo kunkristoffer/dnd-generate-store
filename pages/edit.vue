15539a848c9f26d0c2924479e90e729ffd6df0c1
kristoffer <kunkristoffer@gmail.com>
2024-10-13 15:27:42 +0200

    Added page for editing items

---
+++ pages/edit.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const showCreating = ref(false)
+</script>
+<template>
+  <main class="relative grid place-content-center flex-1 p-4 gap-4 bg-slate-700 text-white">
+    <span class="flex justify-between">
+      <ItemsListSearch />
+      <button class="py-2 px-4 bg-green-600 hover:bg-green-500 active:scale-105 rounded-lg" @click.prevent="showCreating = !showCreating">
+        create
+      </button>
+    </span>
+    <ItemsList />
+    <ItemsCreate v-if="showCreating"/>
+  </main>
+</template>
+<style scoped>
+
+</style>
