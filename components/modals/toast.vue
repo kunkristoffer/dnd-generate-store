3bee90cf56c7bf89433266452b2228992d6c423b
kristoffer <kunkristoffer@gmail.com>
2024-10-21 19:18:33 +0200

    Added a toast component

---
+++ components/modals/toast.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  const { message, severity } = defineProps<{message:string, severity: 'info'|'warning'|'error'|'success'}>()
+  const visible = ref(false)
+  onUpdated(() => {
+    visible.value = true
+    setTimeout(() => {
+      visible.value = false
+    }, 5000)
+  })
+</script>
+<template>
+  <div v-if="visible" class="fixed right-8 bottom-4 px-8 py-2 border rounded-lg bg-slate-500" :class="severity">
+    {{ message }}
+  </div>
+</template>
+<style scoped>
+  .info {
+    background-color: lightblue;
+  }
+  .warning {
+    background-color: yellow;
+  }
+  .error {
+    background-color: red;
+    color: white;
+    border-color: white;
+  }
+  .success {
+    background-color: greenyellow;
+  }
+</style>
