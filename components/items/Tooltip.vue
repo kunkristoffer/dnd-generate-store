0d4d93dba56a4c1e88bd56fe45fc84939de0ad61
kristoffer <kunkristoffer@gmail.com>
2024-10-30 13:03:01 +0100

    added tooltip component

---
+++ components/items/Tooltip.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+<template>
+  <div>
+    hmmm, bruk position fixed og calc mus position
+    send data til tooltip fra props
+  </div>
+</template>
+<style scoped>
+
+</style>
