975c5801fc12e66d1149bf87438f9998c91b7609
kristoffer <kunkristoffer@gmail.com>
2024-10-30 13:41:45 +0100

    Added placeholder logo

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex justify-between items-center bg-slate-800 text-white p-4">
-    logo
+    <img src="/logo.png" alt="Discover Nuxt 3" class="h-12"/>
     <HeaderNavbar />
   </header>
 </template>
