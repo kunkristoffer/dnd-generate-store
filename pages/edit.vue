7456f4ea12b456662556bcc3c026c2b0d9703e7d
kristoffer <kunkristoffer@gmail.com>
2024-10-30 13:03:26 +0100

    Added modal closing emit

--- pages/edit.vue
+++ pages/edit.vue
@@ -1,16 +1,14 @@
-<script setup lang="ts">
-  const showCreating = ref(false)
+<script setup>
+  const showCreate = ref(false)
 </script>
 <template>
   <main class="relative grid place-content-center flex-1 p-4 gap-4 bg-slate-700 text-white">
     <span class="flex justify-between">
       <ItemsListSearch />
-      <button class="py-2 px-4 bg-green-600 hover:bg-green-500 active:scale-105 rounded-lg" @click.prevent="showCreating = !showCreating">
-        create
-      </button>
+      <button class="py-2 px-4 bg-green-600 hover:bg-green-500 active:scale-105 rounded-lg" @click.prevent="showCreate = true">create</button>
     </span>
     <ItemsList />
-    <ItemsCreate v-if="showCreating"/>
+    <ItemsCreate v-if="showCreate" @toggle-visibility="showCreate = false"/>
   </main>
 </template>
 <style scoped>
