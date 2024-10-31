be3921084e6ef63b63975d41c5737688792067ce
kristoffer <kunkristoffer@gmail.com>
2024-10-31 19:11:51 +0100

    moved home link to logo

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex justify-between items-center bg-slate-800 text-white p-4">
-    <img src="/logo.png" alt="Discover Nuxt 3" class="h-12"/>
+    <NuxtLink to="/"><img src="/logo.png" alt="Discover Nuxt 3" class="h-12"/></NuxtLink>
     <HeaderNavbar />
   </header>
 </template>
--- components/header/Navbar.vue
+++ components/header/Navbar.vue
@@ -1,8 +1,7 @@
 <template>
   <nav>
     <ul class="flex gap-4">
-      <li><NuxtLink to="/">Home</NuxtLink></li>
-      <li><NuxtLink to="/shop">Shop</NuxtLink></li>
+      <li><NuxtLink to="/shop">Items</NuxtLink></li>
       <li><NuxtLink to="/edit">Edit</NuxtLink></li>
     </ul>
   </nav>
