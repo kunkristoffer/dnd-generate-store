0520a7a442d71538da33456a402db27a9be8149e
kristoffer <kunkristoffer@gmail.com>
2024-11-01 09:12:19 +0100

    changed header zindex

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between items-center bg-slate-800 text-white p-4">
+  <header class="flex justify-between items-center bg-slate-800 text-white p-4 z-50">
     <NuxtLink to="/"><img src="/logo.png" alt="Discover Nuxt 3" class="h-12"/></NuxtLink>
     <HeaderNavbar />
   </header>
