f9b8a37794ceaf5f9879d8556469c2a1aa6c16c8
kristoffer <kunkristoffer@gmail.com>
2024-11-01 09:10:57 +0100

    css tweaks for centering

--- pages/edit.vue
+++ pages/edit.vue
@@ -2,8 +2,8 @@
   const showCreate = ref(false)
 </script>
 <template>
-  <main class="relative grid place-content-center flex-1 p-4 gap-4 bg-slate-700 text-white">
-    <span class="flex justify-between">
+  <main class="relative flex flex-col items-center flex-1 py-4 gap-4 bg-slate-700 text-white">
+    <span class="flex justify-between items-center w-full max-w-screen-lg max-sm:flex-col max-sm:gap-4">
       <ItemsListSearch />
       <button class="py-2 px-4 bg-green-600 hover:bg-green-500 active:scale-105 rounded-lg" @click.prevent="showCreate = true">create</button>
     </span>
